038a9081fef8197937a0a2f24b1e50f799e6e34c
Youssef97dev <youssefmoussafi97@gmail.com>
2024-10-07 09:58:38 +0100

    change links

--- src/app/page.js
+++ src/app/page.js
@@ -28,9 +28,8 @@ export default function HomePage() {
     {
       id: 3,
       title: "L'ambiance",
-      imageSrc: "/assets/images/salamavid3.jpg",
-      videoSrc:
-        "https://res.cloudinary.com/dz7wroord/video/upload/v1728119307/salamapres_jhkoic.mp4",
+      imageSrc: "/assets/images/salama_13.jpg",
+      videoSrc: "/assets/videos/salamavid3.mp4",
       href: "/events",
     },
   ];
