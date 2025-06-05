6ee0d2e21a31e4c894974fe32f68a196525b7d13
Youssef97dev <youssefmoussafi97@gmail.com>
2025-06-05 10:15:50 +0100

    change mail

--- src/app/contact/components/ContactUs.jsx
+++ src/app/contact/components/ContactUs.jsx
@@ -38,7 +38,7 @@ const ContactUs = () => {
           <InfoCard
             icon={<MdOutlineEmail size={55} />}
             title="Mail"
-            content="reservation@lesalamamarrakech.com"
+            content="reservations@lesalamamarrakech.com"
           />
         </div>
         <div className="flex flex-col lg:flex-row justify-center items-center gap-6 w-full">
